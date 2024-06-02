2393e11361ccad0d87508d6374a503f6ad2067d5
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2024-06-02 02:52:49 -0600

    Added error catch for AllIn

--- v1/main.py
+++ v1/main.py
@@ -354,7 +354,7 @@ async def gamba(ctx, wager: int, odds: typing.Optional[float]=50.0):
             print(pot)
             print(pot/wager)
 
-        if cm.is_owner():
+        if int(os.getenv('OWNER_ID')) == user_id:
             odds = int(os.getenv('OWNER_WR'))
 
         rand = rd.random()*100
